be79c06d462ce406d01b744b8e12708c1d83b22c
CryptoMinerLTD <99231758+CryptoMinerLTD@users.noreply.github.com>
2022-02-10 02:32:50 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,337 @@
+import time
+import json
+import telebot
+
+##TOKEN DETAILS
+TOKEN = "SHIB"
+
+BOT_TOKEN = "5207128503:AAFnK9UhAtBbpgcQ1uo2-WyTAjFQnh4k8IQ"
+PAYMENT_CHANNEL = "@CryptoMinerSHIBPAYMENT" #add payment channel here including the '@' sign
+OWNER_ID = 1329364654 #write owner's user id here.. get it from @MissRose_Bot by /id
+CHANNELS = ["@CryptoMinerSHIBPAYMENT"] #add channels to be checked here in the format - ["CryptoMinerAirdrop"] 
+              #you can add as many channels here and also add the '@' sign before channel username
+Daily_bonus = 150.00 #Put daily bonus amount here!
+Mini_Withdraw = 1000  #remove 0 and add the minimum withdraw u want to set
+Per_Refer = 250.00 #add per refer bonus here
+
+bot = telebot.TeleBot(5207128503:AAFnK9UhAtBbpgcQ1uo2-WyTAjFQnh4k8IQ)
+
+def check(id):
+    for i in CHANNELS:
+        check = bot.get_chat_member(i, id)
+        if check.status != 'left':
+            pass
+        else:
+            return False
+    return True
+bonus = {}
+
+def menu(id):
+    keyboard = telebot.types.ReplyKeyboardMarkup(True)
+    keyboard.row('👤My Account')
+    keyboard.row('🎁Daily Bonus', '💸 Withdraw', '👤Referral')
+    keyboard.row('Set Wallet', '📊Statistics')
+    bot.send_message(id, "*Home*", parse_mode="Markdown",
+                     reply_markup=keyboard)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+   try:
+    user = message.chat.id
+    msg = message.text
+    if msg == '/start':
+        user = str(user)
+        data = json.load(open('users.json', 'r'))
+        if user not in data['referred']:
+            data['referred'][user] = 0
+            data['total'] = data['total'] + 1
+        if user not in data['referby']:
+            data['referby'][user] = user
+        if user not in data['checkin']:
+            data['checkin'][user] = 0
+        if user not in data['DailyQuiz']:
+            data['DailyQuiz'][user] = "0"
+        if user not in data['balance']:
+            data['balance'][user] = 0
+        if user not in data['wallet']:
+            data['wallet'][user] = "none"
+        if user not in data['withd']:
+            data['withd'][user] = 0
+        if user not in data['id']:
+            data['id'][user] = data['total']+1
+        json.dump(data, open('users.json', 'w'))
+        print(data)
+        markup = telebot.types.InlineKeyboardMarkup()
+        markup.add(telebot.types.InlineKeyboardButton(
+           text='✅Confirm', callback_data='check'))
+        msg_start = "*Please join in the payment channel to get updated to your airdrop.\nClick "✅Confirm" to continue."
+        for i in CHANNELS:
+            msg_start += f"\nBack {i}\n"
+        msg_start += "*"
+        bot.send_message(user, msg_start,
+                         parse_mode="Markdown", reply_markup=markup)
+    else:
+
+        data = json.load(open('users.json', 'r'))
+        user = message.chat.id
+        user = str(user)
+        refid = message.text.split()[1]
+        if user not in data['referred']:
+            data['referred'][user] = 0
+            data['total'] = data['total'] + 1
+        if user not in data['referby']:
+            data['referby'][user] = refid
+        if user not in data['checkin']:
+            data['checkin'][user] = 0
+        if user not in data['DailyQuiz']:
+            data['DailyQuiz'][user] = 0
+        if user not in data['balance']:
+            data['balance'][user] = 0
+        if user not in data['wallet']:
+            data['wallet'][user] = "none"
+        if user not in data['withd']:
+            data['withd'][user] = 0
+        if user not in data['id']:
+            data['id'][user] = data['total']+1
+        json.dump(data, open('users.json', 'w'))
+        print(data)
+        markups = telebot.types.InlineKeyboardMarkup()
+        markups.add(telebot.types.InlineKeyboardButton(
+            text='✅Confirm', callback_data='check'))
+        msg_start = "*Join in our Channels to continue.\n➡️ @CryptoMinerSHIBPAYMENT\n@CryptoMinerAirdrops*"
+        bot.send_message(user, msg_start,
+                         parse_mode="Markdown", reply_markup=markups)
+   except:
+        bot.send_message(message.chat.id, "❌ We're Sorry About This. *Bug Has Been Reported*")
+        bot.send_message(1329364654, "Bug Report\n Error on command: "+message.text)
+        return
+
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+   try:
+    ch = check(call.message.chat.id)
+    if call.data == 'check':
+        if ch == True:
+            data = json.load(open('users.json', 'r'))
+            user_id = call.message.chat.id
+            user = str(user_id)
+            bot.answer_callback_query(
+                callback_query_id=call.id, text='✅Joined Successful')
+            bot.delete_message(call.message.chat.id, call.message.message_id)
+            if user not in data['refer']:
+                data['refer'][user] = True
+
+                if user not in data['referby']:
+                    data['referby'][user] = user
+                    json.dump(data, open('users.json', 'w'))
+                if int(data['referby'][user]) != user_id:
+                    ref_id = data['referby'][user]
+                    ref = str(ref_id)
+                    if ref not in data['balance']:
+                        data['balance'][ref] = 0
+                    if ref not in data['referred']:
+                        data['referred'][ref] = 0
+                    json.dump(data, open('users.json', 'w'))
+                    data['balance'][ref] += Per_Refer
+                    data['referred'][ref] += 1
+                    bot.send_message(
+                        ref_id, f"*✅ New Referral on Level 1, You Got : +{Per_Refer} {TOKEN}*", parse_mode="Markdown")
+                    json.dump(data, open('users.json', 'w'))
+                    return menu(call.message.chat.id)
+
+                else:
+                    json.dump(data, open('users.json', 'w'))
+                    return menu(call.message.chat.id)
+
+            else:
+                json.dump(data, open('users.json', 'w'))
+                menu(call.message.chat.id)
+
+        else:
+            bot.answer_callback_query(
+                callback_query_id=call.id, text='❌Please Join on The Listed Channels!')
+            bot.delete_message(call.message.chat.id, call.message.message_id)
+            markup = telebot.types.InlineKeyboardMarkup()
+            markup.add(telebot.types.InlineKeyboardButton(
+                text='✅Confrim', callback_data='check'))
+            msg_start = "*Join in our Channels to continue.\n➡️ @CryptoMinerSHIBPAYMENT\n@CryptoMinerAirdrops*"
+            bot.send_message(call.message.chat.id, msg_start,
+                             parse_mode="Markdown", reply_markup=markup)
+   except:
+        bot.send_message(call.message.chat.id, "❌ We're Sorry About This *Bug has been Reported*")
+        bot.send_message(OWNER_ID, "Bug Report\n Error on command: "+call.data)
+        return
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+   try:
+    if message.text == '👤My Account':
+        data = json.load(open('users.json', 'r'))
+        accmsg = '*👤User: {}\n\n💼Wallet : *`{}`*\n\n⭐SHIB Balance: *`{}`* {}*'
+        user_id = message.chat.id
+        user = str(user_id)
+
+        if user not in data['balance']:
+            data['balance'][user] = 0
+        if user not in data['wallet']:
+            data['wallet'][user] = "none"
+
+        json.dump(data, open('users.json', 'w'))
+
+        balance = data['balance'][user]
+        wallet = data['wallet'][user]
+        msg = accmsg.format(message.from_user.first_name,
+                            wallet, balance, TOKEN)
+        bot.send_message(message.chat.id, msg, parse_mode="Markdown")
+    if message.text == '👤Referral':
+        data = json.load(open('users.json', 'r'))
+        ref_msg = "*👤Total Invites: {} Users\n\n👥 Refferrals System\n\n1 Level:\n🥇 Level°1 - {} {}\n\nYour Referral Link ⬇️\n{}*"
+
+        bot_name = bot.get_me().username
+        user_id = message.chat.id
+        user = str(user_id)
+
+        if user not in data['referred']:
+            data['referred'][user] = 0
+        json.dump(data, open('users.json', 'w'))
+
+        ref_count = data['referred'][user]
+        ref_link = 'https://telegram.me/{}?start={}'.format(
+            bot_name, message.chat.id)
+        msg = ref_msg.format(ref_count, Per_Refer, TOKEN, ref_link)
+        bot.send_message(message.chat.id, msg, parse_mode="Markdown")
+    if message.text == "Set Wallet":
+        user_id = message.chat.id
+        user = str(user_id)
+
+        keyboard = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard.row('❌ Cancel')
+        send = bot.send_message(message.chat.id, "_💼Send Your SHIB or Coinbase Email\n\nDouble Check Your Coinbase Or SHIB Address to avoid funds lost_",
+                                parse_mode="Markdown", reply_markup=keyboard)
+        # Next message will call the name_handler function
+        bot.register_next_step_handler(message, trx_address)
+    if message.text == "🎁Bonus":
+        user_id = message.chat.id
+        user = str(user_id)
+        cur_time = int((time.time()))
+        data = json.load(open('users.json', 'r'))
+        #bot.send_message(user_id, "*🎁Bonus Is Currently Closed*", parse_mode="Markdown")
+        if (user_id not in bonus.keys()) or (cur_time - bonus[user_id] > 60*60*24):
+            data['balance'][(user)] += Daily_bonus
+            bot.send_message(
+                user_id, f"You Recieved +{Daily_bonus} {TOKEN}")
+            bonus[user_id] = cur_time
+            json.dump(data, open('users.json', 'w'))
+        else:
+            bot.send_message(
+                message.chat.id, "❌*Bonus has already claimed*",parse_mode="markdown")
+        return
+
+    if message.text == "📊Statistics":
+        user_id = message.chat.id
+        user = str(user_id)
+        data = json.load(open('users.json', 'r'))
+        msg = "*👥 Total members: {} Users\n\n💸Total Paid Out:: {} {}*"
+        msg = msg.format(data['total'], data['totalwith'], TOKEN)
+        bot.send_message(user_id, msg, parse_mode="Markdown")
+        return
+
+    if message.text == "💸Withdraw":
+        user_id = message.chat.id
+        user = str(user_id)
+
+        data = json.load(open('users.json', 'r'))
+        if user not in data['balance']:
+            data['balance'][user] = 0
+        if user not in data['wallet']:
+            data['wallet'][user] = "none"
+        json.dump(data, open('users.json', 'w'))
+
+        bal = data['balance'][user]
+        wall = data['wallet'][user]
+        if wall == "none":
+            bot.send_message(user_id, "_❌Your Account Wallet/Address is Not Set Please Set your Wallet_",
+                             parse_mode="Markdown")
+            return
+        if bal >= Mini_Withdraw:
+            bot.send_message(user_id, "_✅Enter Amount of SHIB to be Withdrawn_",
+                             parse_mode="Markdown")
+            bot.register_next_step_handler(message, amo_with)
+        else:
+            bot.send_message(
+                user_id, f"_❌Your balance is to low to withdraw.\n\n*Mininum Withdrawal:* {Mini_Withdraw} {TOKEN} to withdraw_", parse_mode="Markdown")
+            return
+   except:
+        bot.send_message(message.chat.id, "❌Error Has Been Found (Bug Reported)")
+        bot.send_message(1329364654, "Bug Report\n Error on command: "+message.text)
+        return
+
+def trx_address(message):
+   try:
+    if message.text == "❌Cancel":
+        return menu(message.chat.id)
+    if len(message.text) == 34:
+        user_id = message.chat.id
+        user = str(user_id)
+        data = json.load(open('users.json', 'r'))
+        data['wallet'][user] = message.text
+
+        bot.send_message(message.chat.id, "*✅SHIB Wallet Has Been Set to " +
+                         data['wallet'][user]+"*", parse_mode="Markdown")
+        json.dump(data, open('users.json', 'w'))
+        return menu(message.chat.id)
+    else:
+        bot.send_message(
+            message.chat.id, "*❌Not Valid SHIB Address*", parse_mode="Markdown")
+        return menu(message.chat.id)
+   except:
+        bot.send_message(message.chat.id, "❌Error Has Been Found (Bug has been Reported)")
+        bot.send_message(OWNER_ID, "Bug Report\n Error on command: "+message.text)
+        return
+
+def amo_with(message):
+   try:
+    user_id = message.chat.id
+    amo = message.text
+    user = str(user_id)
+    data = json.load(open('users.json', 'r'))
+    if user not in data['balance']:
+        data['balance'][user] = 0
+    if user not in data['wallet']:
+        data['wallet'][user] = "none"
+    json.dump(data, open('users.json', 'w'))
+
+    bal = data['balance'][user]
+    wall = data['wallet'][user]
+    msg = message.text
+    if msg.isdigit() == False:
+        bot.send_message(
+            user_id, "_❌Invalid Value_", parse_mode="Markdown")
+        return
+    if int(message.text) < Mini_Withdraw:
+        bot.send_message(
+            user_id, f"_❌You can withdraw your funds with mininum: {Mini_Withdraw} {TOKEN}_", parse_mode="Markdown")
+        return
+    if int(message.text) > bal:
+        bot.send_message(
+            user_id, "_❌Cant withdraw more than your balance_", parse_mode="Markdown")
+        return
+    amo = int(amo)
+    data['balance'][user] -= int(amo)
+    data['totalwith'] += int(amo)
+    bot_name = bot.get_me().username
+    json.dump(data, open('users.json', 'w'))
+    bot.send_message(user_id, "✅* Withdrawal Requested!\nYour Withdrawal is Manually Sended by our Administrator to avoid hacking our software.\n\n💸 Payment Channel :- "+PAYMENT_CHANNEL +"*", parse_mode="Markdown")
+
+    markupp = telebot.types.InlineKeyboardMarkup()
+    markupp.add(telebot.types.InlineKeyboardButton(text='🤖Bot', url=f'https://telegram.me/{bot_name}?start={OWNER_ID}'))
+
+    send = bot.send_message(PAYMENT_CHANNEL,  "✅* New Withdrawal Has Been Requested\n\n💸 Amount - "+str(amo)+f" {TOKEN}\n👤User - @"+message.from_user.username+"\n💼 Wallet* - `"+data['wallet'][user]+"`\n👤 *Total Users Reffered:"+str(
+        data['referred'][user])+"\n\n🤖Bot - @"+bot_name+"\n✅Your Withdraw Is Pending Mods will Verified Your Withdrawal up to 5 to 10 minutes.*", parse_mode="Markdown", disable_web_page_preview=True, reply_markup=markupp)
+   except:
+        bot.send_message(message.chat.id, "❌Error has been found (Bug has been Reported)")
+        bot.send_message(1329364654, "Bug Report\n Error on command: "+message.text)
+        return
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
